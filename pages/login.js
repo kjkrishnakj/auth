989bd46dba2b3fe4bb57a54d8e30b465e542955a
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-07-07 12:49:55 +0530

    v10.1

--- pages/login.js
+++ pages/login.js
@@ -76,7 +76,7 @@ const Login = ({darkMode}) => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit} className="space-y-6" method="POST">
             <div>
-              <label htmlFor="email" className={`block text-sm font-medium leading-6 ${darkMode ? ' text-white' : ' text-gray-900'} `}>Email address</label>
+              <label htmlFor="email" className={`block text-sm font-medium leading-6  text-white `}>Email address</label>
               <div className="mt-2">
                 <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
@@ -84,7 +84,7 @@ const Login = ({darkMode}) => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className={`block text-sm font-medium leading-6 ${darkMode ? ' text-white' : ' text-gray-900'} `}>Password</label>
+                <label htmlFor="password" className={`block text-sm font-medium leading-6   text-white `}>Password</label>
               </div>
               <div className="mt-2">
                 <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
