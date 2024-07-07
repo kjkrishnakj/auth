7a130ab98b51522b55c8722be8422b37ac05debf
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-07-07 12:51:08 +0530

    v10.2

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,15 +2,25 @@ import connectDb from "../../middleware/mongoose";
 import User from "../../models/User";
 
 const handler = async (req, res) => {
-  if (req.method == 'POST') {
-    let u = new User(req.body)
-    await u.save();
-    res.status(200).json({ success: "success" });
-  }
-  else {
-    res.status(400).json({ error: "error" });
+  if (req.method === 'POST') {
+    try {
+      
+      let newUser = new User(req.body);
+      
+      
+      await newUser.save();
 
+      
+      res.status(201).json({ success: "User created successfully" });
+    } catch (error) {
+      
+      console.error("Error saving user:", error);
+      res.status(500).json({ error: "Failed to save user" });
+    }
+  } else {
+    
+    res.status(400).json({ error: "Bad request" });
   }
 }
 
-export default connectDb(handler)
+export default connectDb(handler);
